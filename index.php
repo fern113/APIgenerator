e1d22bf3fe39dff1620d83b6f7aa4c674662abf1
Fern <fern1972@gmail.com>
2017-03-15 01:01:48 +0000

    php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+            <?php for ($i = 0;$i<4 ; $i++){
+                echo "<p> hello world</p>"
+            }
+            ?>
+        </body>
+    </head>
+</html>
